32efe393123dce52386285583ace5c6db274f3aa
Alexander Wisowaty <alexander@wisowaty.com>
2018-08-09 17:07:58 +0200

    styled

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  # get 'cocktails', to: 'cocktails#index', as: :cocktails
-  # get 'cocktails/new', to: 'cocktails#new', as: :new_cocktail
-  # get 'cocktails/:id', to: 'cocktails#show', as: :cocktail
-  # post 'cocktails', to: 'cocktails#create'
-
-  # get 'cocktails/:cocktail_id/doses/new', to: 'doses#new', as: :new_dose
-  # post 'cocktails/:cocktail_id/doses', to: 'doses#create'
   resources :cocktails do 
     resources :doses, only: [:new, :create]
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,6 @@ Ingredient.destroy_all
 data = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
 parsed = JSON.parse(data)
 
-ingredients = []
-
 puts 'Seed DB'
 parsed['drinks'].each do |line|
   ing_name = line['strIngredient1']
